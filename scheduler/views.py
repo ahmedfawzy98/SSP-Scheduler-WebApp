a17ff82e6f4f622d9a9c2a8d7f877567565280ae
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-02-27 22:07:35 +0200

    POST path identification

--- scheduler/views.py
+++ scheduler/views.py
@@ -18,6 +18,7 @@ def select_courses(request):
 
 
 def index(request):
+    print(request.POST.get("submit"))
     # if request.method == "GET":
     #     dict = {}
     #     input = Input()
@@ -100,7 +101,11 @@ def index(request):
         controller = Controller()
         input = Input()
         allcourses = input.courses
-        if(len(courses) == 0):
+        if request.POST.get("submit") == "selection":
+            if len(courses) > 0:
+                clean_priority(courses)
+                courses.clear()
+
             for course in allcourses:
                 if request.POST.get(course.name) == "on":
                     courses.append(course)
@@ -116,8 +121,12 @@ def index(request):
                 i += 1
             return render(request, 'schedule.html', context=dict)
 
-        else:
+        elif request.POST.get("submit") == "generation":
             clean_priority(courses)
+            # courses.clear()
+            # for course in allcourses:
+            #     if request.POST.get(course.name) == "on":
+            #         courses.append(course)
             dict["courses"] = courses
             dict["coursesNum"] = len(courses)
             for course in courses:
@@ -130,8 +139,6 @@ def index(request):
                             if (pr[1] == inst.name):
                                 inst.priority = int(request.POST.get(course.name + "Pr"))
                                 course.priority = int(request.POST.get(course.name + "Pr"))
-            # courses[2].priority = 5
-            # courses[2].instructors[1].priority = 5
             controller.courses = courses
             controller.makeSchedule()
             schedule = controller.schedule.schedule
@@ -161,6 +168,79 @@ def index(request):
             # dict[]
             # del controller
             return render(request, 'schedule.html', context=dict)
+
+
+
+        # priority = []
+        # dict = {}
+        # controller = Controller()
+        # input = Input()
+        # allcourses = input.courses
+        # if(len(courses) == 0):
+        #     for course in allcourses:
+        #         if request.POST.get(course.name) == "on":
+        #             courses.append(course)
+        #     dict["courses"] = courses
+        #     dict["coursesNum"] = len(courses)
+        #     courses.sort(key=attrgetter('name'))
+        #     i = 0
+        #     while i < 6:
+        #         j = 0
+        #         while j < 12:
+        #             dict['p' + str(i) + '_' + str(j)] = "<td></td>"
+        #             j += 1
+        #         i += 1
+        #     return render(request, 'schedule.html', context=dict)
+        #
+        # else:
+        #     clean_priority(courses)
+        #     # courses.clear()
+        #     # for course in allcourses:
+        #     #     if request.POST.get(course.name) == "on":
+        #     #         courses.append(course)
+        #     dict["courses"] = courses
+        #     dict["coursesNum"] = len(courses)
+        #     for course in courses:
+        #         if (request.POST.get(course.name) != "Select Instructor"):
+        #             priority.append((course.name, request.POST.get(course.name)))
+        #     for pr in priority:
+        #         for course in courses:
+        #             if (pr[0] == course.name):
+        #                 for inst in course.instructors:
+        #                     if (pr[1] == inst.name):
+        #                         inst.priority = int(request.POST.get(course.name + "Pr"))
+        #                         course.priority = int(request.POST.get(course.name + "Pr"))
+        #     # courses[2].priority = 5
+        #     # courses[2].instructors[1].priority = 5
+        #     controller.courses = courses
+        #     controller.makeSchedule()
+        #     schedule = controller.schedule.schedule
+        #     i = 0
+        #     while i < 6:
+        #         j = 0
+        #         while j < 12:
+        #             if schedule[i][j] is not None:
+        #
+        #                 if schedule[i][j].periodType == "Lecture":
+        #                     dict['p' + str(i) + '_' + str(j)] = "<td bgcolor='#FFE9E7' colspan='" + str(
+        #                         schedule[i][j].length) + "'>" + schedule[i][j].courseName + "<br>" + schedule[i][
+        #                                                             j].instName + "</td>"
+        #                 elif schedule[i][j].periodType == "Tut":
+        #                     dict['p' + str(i) + '_' + str(j)] = "<td bgcolor='#d1e7f7' >" + schedule[i][
+        #                         j].courseName + "<br>" + schedule[i][j].instName + "</td>"
+        #                 else:
+        #                     dict['p' + str(i) + '_' + str(j)] = "<td bgcolor='#BDFFFF'>" + schedule[i][
+        #                         j].courseName + "<br>" + schedule[i][j].instName + "</td>"
+        #                 j += schedule[i][j].length
+        #             else:
+        #                 dict['p' + str(i) + '_' + str(j)] = "<td></td>"
+        #                 j += 1
+        #
+        #         i += 1
+        #     courses.sort(key=attrgetter('name'))
+        #     # dict[]
+        #     # del controller
+        #     return render(request, 'schedule.html', context=dict)
 
 
 
