92c252730fa0431b99e8bf21b360ea4e57539c41
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-05-22 04:53:00 +0200

    multiple tut/lab support (bad runtime)

--- scheduler/Classes/Node.py
+++ scheduler/Classes/Node.py
@@ -2,22 +2,25 @@ from scheduler.Classes.Schedule import Schedule
 
 
 class Node:
-    def __init__(self, data, sch=None):
+    def __init__(self, data, sch=None, iTut=-1,iLab=-1):
         self.children = []
         self.parent = None
         self.data = data
         self.schedule = Schedule()
         if sch is not None:
             self.schedule.clone(sch)
+        self.tutNum = iTut
+        self.labNum = iLab
         self.add_to_schedule()
 
-    def add_child(self, data):
-        child = Node(data, self.schedule)
+
+    def add_child(self, data, iTut, iLab):
+        child = Node(data, self.schedule, iTut, iLab)
         child.parent = self
         self.children.append(child)
 
     @staticmethod
-    def check_clash(group, sch):
+    def check_clash(group, sch , iTut, iLab):
         if sch.check_clash(group.lecture):
             return True
 
@@ -25,11 +28,11 @@ class Node:
             if sch.check_clash(group.exLecture):
                 return True
 
-        for i in range(len(group.tutorials)):
-            if sch.check_clash(group.tutorials[i]):
+        if iTut != -1:
+            if sch.check_clash(group.tutorials[iTut]):
                 return True
-        for i in range(len(group.labs)):
-            if sch.check_clash(group.labs[i]):
+        if iLab != -1:
+            if sch.check_clash(group.labs[iLab]):
                 return True
         return False
 
@@ -37,10 +40,10 @@ class Node:
         self.schedule.add_period(self.data.lecture)
         if self.data.exLecture is not None:
             self.schedule.add_period(self.data.exLecture)
-        for i in range(len(self.data.tutorials)):
-            self.schedule.add_period(self.data.tutorials[i])
-        for i in range(len(self.data.labs)):
-            self.schedule.add_period(self.data.labs[i])
+        if self.tutNum != -1:
+            self.schedule.add_period(self.data.tutorials[self.tutNum])
+        if self.labNum != -1:
+            self.schedule.add_period(self.data.labs[self.labNum])
 
     def all_available(self):
         current = self
--- scheduler/Controller/Controller.py
+++ scheduler/Controller/Controller.py
@@ -2,6 +2,7 @@ from operator import attrgetter
 from scheduler.Classes.Node import Node
 from scheduler.Classes.Schedule import Schedule
 from scheduler.Controller.Input import Input
+import time
 
 
 class Controller:
@@ -20,29 +21,43 @@ class Controller:
             course.instructors.sort(key=attrgetter('priority'), reverse=True)
         for i in range(len(self.courses[0].instructors)):
             for j in range(len(self.courses[0].instructors[i].groups)):
-                n = Node(self.courses[0].instructors[i].groups[j])
-                n.schedule.add_to_priority(self.courses[0].instructors[i].priority)
-                self.level.append(n)
+                tutsNum = len(self.courses[0].instructors[i].groups[j].tutorials)
+                labsNum = len(self.courses[0].instructors[i].groups[j].labs)
+                tutsInd = list(range(0, tutsNum)) if tutsNum > 0 else [-1]
+                labsInd = list(range(0, labsNum)) if labsNum > 0 else [-1]
+                for iTut in range(len(tutsInd)):
+                    for iLab in range(len(labsInd)):
+                        n = Node(self.courses[0].instructors[i].groups[j],iTut=tutsInd[iTut],iLab=labsInd[iLab])
+                        n.schedule.add_to_priority(self.courses[0].instructors[i].priority)
+                        self.level.append(n)
         self.levels.append(self.level)
+        start_time = time.time()
         for i in range(1, len(self.courses)):
             self.levels.append(list())
             for j in range(len(self.courses[i].instructors)):
                 for k in range(len(self.courses[i].instructors[j].groups)):
                     for o in range(len(self.levels[i - 1])):
-                        # check if the current group doesn't clash with the current tree branch
-                        if not self.levels[i - 1][o].check_clash(self.courses[i].instructors[j].groups[k],
-                                                                 self.levels[i - 1][o].schedule):
-                            # add the group to the current branch
-                            self.levels[i - 1][o].add_child(self.courses[i].instructors[j].groups[k])
-                            # update the last added child total schedule's priority
-                            self.levels[i - 1][o].children[-1].schedule.add_to_priority(
-                                self.courses[i].instructors[j].priority +
-                                self.levels[i - 1][o].children[-1].parent.get_total_priority())
-                            # add the last added child to the current level
-                            self.levels[i].append(self.levels[i - 1][o].children[-1])
-                            # if the current course is the last add the last added child to the completed array
-                            if i == len(self.courses) - 1:
-                                self.completed.append(self.levels[i - 1][o].children[-1])
+                        tutsNum = len(self.courses[i].instructors[j].groups[k].tutorials)
+                        labsNum = len(self.courses[i].instructors[j].groups[k].labs)
+                        tutsInd = list(range(0,tutsNum)) if tutsNum > 0 else [-1]
+                        labsInd = list(range(0,labsNum)) if labsNum > 0 else [-1]
+                        for iTut in range(len(tutsInd)):
+                            for iLab in range(len(labsInd)):
+                                # check if the current group doesn't clash with the current tree branch
+                                if not self.levels[i - 1][o].check_clash(self.courses[i].instructors[j].groups[k],
+                                                                         self.levels[i - 1][o].schedule,tutsInd[iTut],labsInd[iLab]):
+                                    # add the group to the current branch
+                                    self.levels[i - 1][o].add_child(self.courses[i].instructors[j].groups[k],tutsInd[iTut],labsInd[iLab])
+                                    # update the last added child total schedule's priority
+                                    self.levels[i - 1][o].children[-1].schedule.add_to_priority(
+                                        self.courses[i].instructors[j].priority +
+                                        self.levels[i - 1][o].children[-1].parent.get_total_priority())
+                                    # add the last added child to the current level
+                                    self.levels[i].append(self.levels[i - 1][o].children[-1])
+                                    # if the current course is the last add the last added child to the completed array
+                                    if i == len(self.courses) - 1:
+                                        self.completed.append(self.levels[i - 1][o].children[-1])
+        print("FOR LOOP : --- %s seconds ---" % (time.time() - start_time))
         self.completed.sort(key=attrgetter('schedule.priorityValue'), reverse=True)
         self.completedPriorityDuplicate = [self.completed[i] for i in range(0,len(self.completed))
                                            if self.completed[0].get_total_priority()
@@ -52,21 +67,21 @@ class Controller:
         self.schedule = self.completedPriorityDuplicate[0].schedule
         perfect = self.completedPriorityDuplicate[0]
         # getting alternative schedules
-        for i in range(len(self.courses)):
-            self.completedPriorityDuplicate.clear()
-            if i != 0:
-                perfect = perfect.parent
-            perfect.data.available = False
-            # iterate until a schedule without an unavailable group is found
-            for j in range(len(self.completed)):
-                if self.completed[j].all_available():
-                    # add the first found all-available schedule and its priority duplicates to completedPriorityD
-                    self.completedPriorityDuplicate = [self.completed[j+k] for k in range(0, len(self.completed)-j-1)
-                                                       if (self.completed[j].get_total_priority()
-                                                       == self.completed[j+k].get_total_priority()
-                                                           and self.completed[j+k].all_available())]
-                    break
-            if len(self.completedPriorityDuplicate) != 0:
-                self.completedPriorityDuplicate.sort(key=attrgetter('schedule.daysTaken'))
-                self.alternatives.append(self.completedPriorityDuplicate[0].schedule)
-            perfect.data.available = True
+        # for i in range(len(self.courses)):
+        #     self.completedPriorityDuplicate.clear()
+        #     if i != 0:
+        #         perfect = perfect.parent
+        #     perfect.data.available = False
+        #     # iterate until a schedule without an unavailable group is found
+        #     for j in range(len(self.completed)):
+        #         if self.completed[j].all_available():
+        #             # add the first found all-available schedule and its priority duplicates to completedPriorityD
+        #             self.completedPriorityDuplicate = [self.completed[j+k] for k in range(0, len(self.completed)-j-1)
+        #                                                if (self.completed[j].get_total_priority()
+        #                                                == self.completed[j+k].get_total_priority()
+        #                                                    and self.completed[j+k].all_available())]
+        #             break
+        #     if len(self.completedPriorityDuplicate) != 0:
+        #         self.completedPriorityDuplicate.sort(key=attrgetter('schedule.daysTaken'))
+        #         self.alternatives.append(self.completedPriorityDuplicate[0].schedule)
+        #     perfect.data.available = True
