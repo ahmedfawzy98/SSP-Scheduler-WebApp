19a4df6bea834dddd050dafecc696bd72bcb4193
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-04-10 13:09:02 +0000

    added domain to allowed hosts

--- sspscheduler/settings.py
+++ sspscheduler/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ed@xj@wo5^lpn%y_er8&y@+u%p0gf)uv0odf5hh6%e80-k$3n0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '46.101.128.103', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '46.101.128.103', 'localhost', 'sspscheduler.xyz', 'www.sspscheduler.xyz']
 
 
 # Application definition
