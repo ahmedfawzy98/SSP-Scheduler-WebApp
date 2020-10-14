0353ccc4e7801f7aced301022f31d758d3fe011b
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-10-15 01:57:30 +0200

    postgresql if DEBUG is false

--- sspscheduler/settings.py
+++ sspscheduler/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'sspscheduler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'sspscheduler',
+            'USER': 'scheduleruser',
+            'PASSWORD': '5740952asd',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
