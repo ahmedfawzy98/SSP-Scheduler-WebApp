6680eb82f2f97e3856dd760df2b10c1752ddfae9
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-10-15 03:17:00 +0200

    changed back to sqlite

--- sspscheduler/settings.py
+++ sspscheduler/settings.py
@@ -77,24 +77,12 @@ WSGI_APPLICATION = 'sspscheduler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'sspscheduler',
-            'USER': 'scheduleruser',
-            'PASSWORD': '5740952asd',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
